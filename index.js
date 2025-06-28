08d9d386620f52c81817502859cd7362fbb70fcf
Juan Camilo Florez <jcflorezv@unal.edu.co>
2025-06-28 17:51:26 -0500

    loop and print ava dates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {Builder, Browser, By} = require('selenium-webdriver');
 
-(async function helloSelenium() {
+async function helloSelenium() {
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
   await driver.get('https://www.karlsruhe.de/stadt-rathaus/service-buergerinformation/terminvereinbarung');
@@ -15,10 +15,10 @@ const {Builder, Browser, By} = require('selenium-webdriver');
   await driver.findElement(By.xpath('//*[@id="sd_2b5c2344-0a6d-47f1-a6c4-6656b2b1adda"]/li/div/span[3]')).click();
   // eAT Bestellung bei neuem/geänderten Pass (Übertrag)
   await driver.findElement(By.xpath('//*[@id="sd_01eeeb8c-7a50-454c-b8e9-bed990ab7ced"]/li/div/span[3]')).click();
-  
+
   // Move to appointment screen
   await driver.findElement(By.css('#forward-service')).click();
-  
+
   // select appointment div
   const appoholder = await driver.findElement(By.xpath('//*[@id="appointment_holder"]'));
   // document.querySelector('//*[@id="appointment_holder"]');
@@ -29,13 +29,21 @@ const {Builder, Browser, By} = require('selenium-webdriver');
     console.log(date);
   }
 
-    
+  await driver.close();
+}
 
 
+function sleep() {
+  return new Promise(resolve => setTimeout(resolve, 300_000));
+}
 
+async function demo() {
+  while(true) {
+    console.log(new Date())
+    await helloSelenium();
+    await sleep();
+    console.log('Done');
+  }
+}
 
-  
-
-  
-  // await driver.quit();
-})();
+demo();
